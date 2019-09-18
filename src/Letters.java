ded2fc4ba100683c76824913c343c7386a0200ff
GAFUR <GAFUR@192.168.0.29>
2019-09-18 16:26:54 -0700

    1-2 Letters completed homework.

--- src/Letters.java
+++ src/Letters.java
@@ -2,8 +2,74 @@
 public class Letters {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		DearParents();
+		GradesAreGreat();
+		Classes();
+		Hobbies();
+		Sincerely();
+		Name();
+		Blank();
+		DearFriends();
+		LoveLife();
+		Classes();
+		Hobbies();
+		Sincerely();
+		Name();
+		Blank();
+		DearTeacher();
+		Future();
+		GradesAreGreat();
+		Classes();
+		Hobbies();
+		Sincerely();
+		Name();
+		}
+	public static void Blank() {
+		System.out.println();
 	}
-
+	public static void DearParents(){
+		System.out.println("	Dear, Mom and Dad,");
+	}
+	public static void GradesAreGreat() {
+		System.out.println("My grades at school are A's except one class is a B.");
+		System.out.println("I have a B in my Geometry class, which is becasue a couple of reasons.");
+		System.out.println("I did not understand all the assignments so some homework assignments are lower grade.");
+		System.out.println("Which is fine because I understand everything now.");
+	}
+	public static void Classes() {
+		System.out.println("Usually Kids who don't take special classes to get more credits,");
+		System.out.println("they usually have a total of 6 classes.");
+		System.out.println("I personally have one more class becasue I am interested in a certain subject, which is AP Computer Science.");
+		System.out.println("Usually Kids also just take the required classes for graduation.");
+	}
+	public static void Hobbies() {
+		System.out.println("On my free time, when I have it, I enjoy spending time with me family, friends and others.");
+		System.out.println("I enjoy spending time with me family by playing games, talking, and also just being with them.");
+		System.out.println("With my friends I do the same as my family, spend time with them and have fun.");
+		System.out.println("I dont have alot free time due to school and homework. Whem I have a little bit of free time I use it wisely.");
+	}
+	public static void Sincerely() {
+		System.out.println("\n	Sincerely,");
+	}
+	public static void Name() {
+		System.out.println("		Matt Gafurov");
+	}
+	public static void DearFriends() {
+		System.out.println("	Dear Friends of Mine,");
+	}
+	public static void LoveLife() {
+		System.out.println("I currently don't have any Love Life.");
+		System.out.println("I never had a Love Life, and never will.");
+		System.out.println("I only liked a girl and thats it.");
+	}
+	public static void DearTeacher() {
+		System.out.println("	 Dear Teachers,");
+	}
+	public static void Future() {
+		System.out.println(" The plan is after college, after I get my Bachlors Degree, hopefully,");
+		System.out.println("I would get a job, get married, which like stated before it won't happen,");
+		System.out.println("get some kids, build a house, buy some cars, also buy a boat and 4 jetski's,");
+		System.out.println("I enjoy driving jetski's, and live on in life, basiclly live every kids dream.");
+	}
 }
+
